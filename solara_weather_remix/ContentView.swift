a97b7ebe452363896dcfa76d5592802fe9a62f50
Tsalist Agna <tsalistagna29@gmail.com>
2026-04-19 10:13:26 +0800

    Initial Commit

---
+++ solara_weather_remix/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  solara_weather_remix
+//
+//  Created by Baiq Annisa Tsalist Agna on 19/04/26.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ solara_weather_remix/solara_weather_remixApp.swift
@@ -0,0 +1,17 @@
+//
+//  solara_weather_remixApp.swift
+//  solara_weather_remix
+//
+//  Created by Baiq Annisa Tsalist Agna on 19/04/26.
+//
+
+import SwiftUI
+
+@main
+struct solara_weather_remixApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
